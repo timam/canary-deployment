c46c25ce27a4b3ed316a1319bc2d1a69aa57d4c7
Timam <mail@timam.io>
2023-11-27 04:58:25 +0600

    canary poc

---
+++ app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		userAgent := r.Header.Get("User-Agent")
+		if strings.Contains(userAgent, "Firefox") {
+			fmt.Fprint(w, "Testing is allowed\n")
+		} else {
+			fmt.Fprint(w, "Welcome\n")
+		}
+	})
+
+	fmt.Println("Server listening on :8080")
+	http.ListenAndServe(":8080", nil)
+}
