ebf4c974d051bb03e1aff84c14d1ebc8fd5f5424
Timam <mail@timam.io>
2023-11-27 05:23:06 +0600

    002

--- app/main.go
+++ app/main.go
@@ -10,9 +10,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		userAgent := r.Header.Get("User-Agent")
 		if strings.Contains(userAgent, "Firefox") {
-			fmt.Fprint(w, "Testing is allowed\n")
+			fmt.Fprint(w, "Testing is allowed!\n")
 		} else {
-			fmt.Fprint(w, "Welcome\n")
+			fmt.Fprint(w, "Welcome!\n")
 		}
 	})
 
